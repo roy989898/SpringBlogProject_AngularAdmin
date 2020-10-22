822b0909fd693c60a2a14812e23e0db87a566aa1
royuuu <roy989898@gmail.com>
2020-10-22 16:19:50 +0800

    save

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TopBarQuery} from "../../akita/TopBarStateStore/TopBarQuery";
+import {MainLoadingStoreService} from "../../akita/MainLoadingStore/main-loading-store.service";
+import delay from 'await-delay';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +11,17 @@ import {TopBarQuery} from "../../akita/TopBarStateStore/TopBarQuery";
 export class LoginComponent implements OnInit {
 
 
+  constructor(private mainLoadingStoreService: MainLoadingStoreService) {
+  }
+
   ngOnInit(): void {
   }
 
 
-  loginClick(): void {
+  async loginClick(): Promise<void> {
+   /* this.mainLoadingStoreService.updateState(true);
+    await delay(2000);
+    this.mainLoadingStoreService.updateState(false);*/
 
   }
 
